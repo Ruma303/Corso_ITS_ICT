a41630e0ea9756c55e3ab9d7d01d3f1d9f68fd20
Matteo Russo <m.russo@its-ictacademy.com>
2026-01-16 14:56:47 +0100

    20260116

---
+++ Python/20260116/20260116_01_passwords.py
@@ -0,0 +1,124 @@
+"""
+Generazione di una password "semplice"
+1. Generazione di un carattere alfabetico casuale
+"""
+
+from random import randint
+
+numbers="0123456789"
+idx=randint(0, len(numbers))
+
+alfa_lower="abcdefghijklmnopqrstuvwxyz"
+idx=randint(0, len(alfa_lower))
+
+alfa_upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+idx=randint(0, len(alfa_upper))
+
+special_chars="!@#$%&*,;.:-_+/\\|\"'?^§[](){}"
+idx=randint(0, len(special_chars))
+
+"""
+Creare algoritmi per generare una password
+"""
+
+all_chars=alfa_upper+alfa_lower+numbers+special_chars
+
+# Test
+def rand_char(alfa):
+  return alfa[randint(0, len(alfa) - 1)]
+
+"""
+Uso di vincoli:
+1. Es massimo - min tot numeri
+2. Non due caratteri successivi
+3. Non
+"""
+
+def password_generator(alfa, /, # Tipo di alfabeto (es solo numeri, solo speciali etc.)
+
+                      min_len=8, max_len=8, # Lunghezza min - max dell'intera password
+
+                      # Vincoli sui set di caratteri
+                      can_nums = True,
+                      can_chars_lower = True,
+                      can_chars_upper = True,
+                      can_specials = True,
+
+                      # Vincoli lunghezze set di caratteri
+                      min_nums=1, max_nums=1, # Quantità min - max di numeri
+                      min_char_lower=1, max_char_lower=1, # Quantità min - max di caratteri minuscoli
+                      min_char_upper=1, max_char_upper=1, # Quantità min - max di caratteri maiuscoli
+                      min_special=1, max_special=1 # Quantità min - max di caratteri speciali
+                      ):
+
+  current_password_length = max_nums + max_char_lower + max_char_upper + max_special
+  # print(f"Vincoli totali: {current_password_length}")
+
+  if max_len < min_len:
+    print(f"La lunghezza massima {max_len} non può essere inferiore alla lunghezza minima {min_len}")
+    max_len = min_len
+    print(f"La password sarà lunga {max_len} caratteri")
+
+  password_length = randint(min_len, max_len)
+  print(f"Lunghezza casuale password: {password_length}")
+
+  # Gestione delle eccezioni
+  try:
+    if password_length < 8 or min_len < 8:
+      raise Exception(f"Per ragioni di sicurezza la lunghezza della password dev'essere di 8 o più caratteri")
+
+    if current_password_length > max_len:
+        raise Exception(f"L'insieme dei vincoli inseriti {current_password_length} supera la lunghezza della password da generare {max_len}")
+
+    if min_len < current_password_length < max_len:
+      raise Exception(f"L'insieme dei vincoli inseriti {current_password_length} dev'essere compreso tra {min_len} e {max_len}")
+
+  except Exception as e:
+    print()
+    print("=====================")
+    print("Eccezione: ", e)
+    print("=====================")
+    print()
+
+
+
+  # TODO Calcolare se e quanti caratteri per ogni set
+
+  # Set dei numeri
+  # Set dei caratteri minuscoli
+  # Set dei caratteri maiuscoli
+  # Set dei speciali
+
+  # Elenco di lunghezze finali per ogni set
+
+  # Inserire tutti i caratteri in base ai vincoli in una lista
+
+  # Creazione password effettiva
+  password = ""
+
+  # Fare lo shuffle della lista alla fine
+
+  for _ in range(0, password_length):
+    char_type = rand_char(alfa)
+
+    if can_nums is False:
+      continue
+
+
+    password += char_type
+
+
+
+  return password
+
+pass1=password_generator(numbers)
+print(f"\t- Password 1 di {len(pass1)} numeri: {pass1}")
+
+pass2=password_generator(all_chars, min_len=10)
+print(f"\t- Password 2 di {len(pass2)} caratteri: {pass2}")
+
+pass3=password_generator(all_chars, min_len=16, max_len=20)
+print(f"\t- Password 3 di {len(pass3)} caratteri: {pass3}")
+
+pass_err1=password_generator(all_chars, min_len=7, max_len=20)
+print(f"\t- Password 4 di {len(pass_err1)} caratteri: {pass_err1}")
